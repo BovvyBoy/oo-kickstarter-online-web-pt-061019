0f38327059d67a399f55262b6bb347e177f02b1c
Daniel Bovington <d.bovington@gmail.com>
2019-07-02 18:34:52 +0000

    Automatically backed up by Learn

---
+++ lib/project.rb
@@ -0,0 +1,13 @@
+class Project
+  attr_reader :backers, :title
+
+  def initialize(title)
+    @title = title
+    @backers = []
+  end
+
+  def add_backer(backer)
+    @backers << backer
+    backer.back_project(self) unless backer.back_project.include?(self)
+  end
+end
